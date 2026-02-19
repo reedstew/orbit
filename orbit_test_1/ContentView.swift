088a50eb34d14b5807500dd52e2dcf1acf91df2c
reedstew <rcstew120@gmail.com>
2026-02-19 13:47:32 -0500

    Initial Commit

---
+++ orbit_test_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  orbit_test_1
+//
+//  Created by Reed Stewart on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ orbit_test_1/orbit_test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  orbit_test_1App.swift
+//  orbit_test_1
+//
+//  Created by Reed Stewart on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct orbit_test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
